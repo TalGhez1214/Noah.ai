0775f18935a83943710b52c9c3796894549f8140
Nadav Antman Ron <nadav.antmanron@gmail.com>
2025-09-26 11:16:33 +0300

    remove last message

--- main.py
+++ main.py
@@ -340,6 +340,7 @@ async def ask_user(request: AskRequest):
     )
 
     messages = manager.chat()
-
+    messages.pop()
+    
     last_ai = next((m.content for m in reversed(messages) if isinstance(m, AIMessage)), "...")
     return AskResponse(result=last_ai)
